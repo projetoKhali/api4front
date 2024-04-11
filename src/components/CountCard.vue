db0add6fc71354996c1d94961d1364902e0aabd1
JhonatanLop <jhooliveira.lopes@gmail.com>
2024-04-11 00:40:18 -0300

    feat: adicionando componente card com texto e número

---
+++ src/components/CountCard.vue
@@ -0,0 +1,54 @@
+<script setup lang="ts">
+import { defineProps, ref } from 'vue'
+
+const props = defineProps({
+    titulo: String,
+    numero: Number
+})
+
+</script>
+
+<template>
+
+    <div class="count-card">
+        <h3 class="title-card">{{ titulo }}</h3>
+        <h1 class="number-card">{{ numero }}</h1>
+    </div>
+</template>
+
+<style scoped>
+.count-card {
+    display: flex;
+    flex-direction: column;
+    gap: 7;
+
+    margin: 10px;
+
+    width: 253px;
+    height: 137px;
+    border-radius: 9px;
+
+    background-color: #ffffff;
+    box-shadow: 0 5px 0 0 rgba(0, 0, 0, 0.1);;
+}
+
+.title-card {
+    margin: 22px 0 0 0;
+
+    font-family: "Inter", sans-serif;
+    font-optical-sizing: auto;
+    font-weight: 400;
+    font-variation-settings: "slnt" 0;
+    color: #000;
+}
+
+.number-card {
+    margin: 0px 0px 26px 0px;
+    
+    font-family: "Inter", sans-serif;
+    font-optical-sizing: auto;
+    font-weight: 400;
+    font-variation-settings: "slnt" 0;
+    color: #000;
+}
+</style>

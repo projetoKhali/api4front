0933a915f43c71990aea38eb150b4ae4efc0d3a8
Paulo Granthon <paulo.granthon@fatec.sp.gov.br>
2024-04-12 20:23:30 -0300

    feat: create `NavBar` component

---
+++ src/components/NavBar.vue
@@ -0,0 +1,16 @@
+<template>
+  <nav>
+    <router-link :to="'/'">
+      <a>Home</a>
+    </router-link>
+    <router-link :to="'/about'">
+      <a>About</a>
+    </router-link>
+    <div>
+      <div>
+        bla
+      </div>
+    </div>
+  </nav>
+</template>
+

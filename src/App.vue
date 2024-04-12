2116c1d30f3284ea33b4f4439a690af1d94ec20d
Paulo Granthon <paulo.granthon@fatec.sp.gov.br>
2024-04-12 20:26:12 -0300

    feat: add `NavBar` to `App`

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,10 @@
 <script setup lang="ts">
 import Example from './views/Example.vue';
+import NavBar from './components/NavBar.vue';
 </script>
 
 <template>
+  <NavBar />
   <router-view />
 </template>
 

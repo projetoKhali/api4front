cefb001f2c4c28660d621f441643a0e33d9f10f9
Paulo Granthon <paulo.granthon@fatec.sp.gov.br>
2024-04-11 21:18:19 -0300

    feat: create `Home` and `Example` views

--- src/views/Example.vue
+++ src/views/Example.vue
@@ -1,9 +1,17 @@
 <template>
   <h1>Example</h1>
+  <router-link :to="'/'">
+    <a>Back to home</a>
+  </router-link>
 </template>
+<script>
+export default {
+  name: 'Example',
+};
+</script>
 
 <style scoped>
 h1 {
-  color: red;
+  color: magenta;
 }
 </style>
---
+++ src/views/Home.vue
@@ -0,0 +1,17 @@
+<template>
+  <h1>Home</h1>
+  <router-link :to="'/example'">
+    <a>Go to <code>/example</code></a>
+  </router-link>
+</template>
+<script>
+export default {
+  name: 'Home',
+};
+</script>
+
+<style scoped>
+h1 {
+  color: green;
+}
+</style>

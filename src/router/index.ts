2a9cab8ca1549cb2fca188a2b8ee830507a77dd7
Paulo Granthon <paulo.granthon@fatec.sp.gov.br>
2024-04-14 19:55:33 -0300

    chore: add `PartnerDashboard` and `TrackDashboard` views to `router`

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,10 +1,22 @@
 import { createRouter, createWebHistory } from 'vue-router';
+import PartnerDashboard from '../views/PartnerDashboard.vue';
+import TrackDashboard from '../views/TrackDashboard.vue';
 
 const routes = [
   {
     path: '/',
     name: 'Home',
     component: () => {},
+  },
+  {
+    path: '/partner',
+    name: 'Partner',
+    component: PartnerDashboard,
+  },
+  {
+    path: '/track',
+    name: 'Track',
+    component: TrackDashboard,
   },
 ];
 

9576419c8e3b8da07f596af3a2f5d0c9e4daa0ed
Paulo Granthon <paulo.granthon@fatec.sp.gov.br>
2024-04-14 19:53:08 -0300

    chore: remove `Home` and `Example` pages

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,17 +1,10 @@
 import { createRouter, createWebHistory } from 'vue-router';
-import Example from '../views/Example.vue';
-import Home from '../views/Home.vue';
 
 const routes = [
   {
     path: '/',
     name: 'Home',
-    component: () => Home,
-  },
-  {
-    path: '/example',
-    name: 'Example',
-    component: Example,
+    component: () => {},
   },
 ];
 

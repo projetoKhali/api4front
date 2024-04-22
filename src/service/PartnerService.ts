01e9cb9a78c4d846908b3fefb24fd7fcd559f3b3
Nicole Souza <nicolemadeleines@gmail.com>
2024-04-22 20:57:36 -0300

    feat. Partner Service creation

--- src/service/PartnerService.ts
+++ src/service/PartnerService.ts
@@ -1,164 +1,57 @@
-import { PartnerSchema } from '../schemas/partner/Partner';
-
-export async function getDataMocked(): Promise<PartnerSchema[]> {
-  try {
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    const mockedData: any = {
-      name: 'Padberg-Quitzon',
-      location: 'União da Vitória',
-      tracks: [
-        {
-          name: 'SUSA',
-          expertises: [
-            {
-              name: 'Curso SouBom',
-              startDate: '2023-01-01',
-              endDate: null,
-              qualifier: [
-                {
-                  name: 'Oracle Cloud Infrastructure 2023 Sales Specialist',
-                  startDate: '2023-01-01',
-                  endDate: '2023-03-15',
-                },
-                {
-                  name: 'Oracle Analytics 2023 Sales Specialist',
-                  startDate: '2023-03-16',
-                  endDate: '2023-05-30',
-                },
-                {
-                  name: 'Oracle Integration Cloud 2024 Sales Specialist',
-                  startDate: '2023-06-01',
-                  endDate: null,
-                },
-                {
-                  name: 'Data Management 2023 Sales Specialist',
-                  startDate: null,
-                  endDate: null,
-                },
-                {
-                  name: 'Oracle Identity and Access Management 2024 Sales Specialist',
-                  startDate: null,
-                  endDate: null,
-                },
-              ],
-            },
-          ],
-        },
-        {
-          name: 'SELL',
-          expertises: [
-            {
-              name: 'Oracle Cloud Platform',
-              startDate: '2023-01-01',
-              endDate: '2023-12-31',
-              qualifier: [
-                {
-                  name: 'Oracle Cloud Infrastructure 2023 Sales Specialist',
-                  startDate: '2023-01-01',
-                  endDate: '2023-02-28',
-                },
-                {
-                  name: 'Oracle Analytics 2023 Sales Specialist',
-                  startDate: '2023-03-01',
-                  endDate: '2023-04-30',
-                },
-                {
-                  name: 'Oracle Integration Cloud 2024 Sales Specialist',
-                  startDate: '2023-05-01',
-                  endDate: '2023-06-30',
-                },
-                {
-                  name: 'Data Management 2023 Sales Specialist',
-                  startDate: '2023-07-01',
-                  endDate: null,
-                },
-                {
-                  name: 'Oracle Identity and Access Management 2024 Sales Specialist',
-                  startDate: null,
-                  endDate: null,
-                },
-              ],
-            },
-            {
-              name: 'Configure, Price Quote (CPQ)',
-              startDate: '2023-01-01',
-              endDate: null,
-              qualifier: [
-                {
-                  name: 'Oracle Cloud Infrastructure 2023 Sales Specialist',
-                  startDate: '2023-01-01',
-                  endDate: '2023-04-30',
-                },
-                {
-                  name: 'Oracle Analytics 2023 Sales Specialist',
-                  startDate: '2023-05-01',
-                  endDate: '2023-08-31',
-                },
-                {
-                  name: 'Oracle Integration Cloud 2024 Sales Specialist',
-                  startDate: '2023-09-01',
-                  endDate: '2023-10-31',
-                },
-                {
-                  name: 'Data Management 2023 Sales Specialist',
-                  startDate: '2023-11-01',
-                  endDate: null,
-                },
-                {
-                  name: 'Oracle Identity and Access Management 2024 Sales Specialist',
-                  startDate: null,
-                  endDate: null,
-                },
-              ],
-            },
-            {
-              name: 'Sales Automation',
-              startDate: '2023-01-01',
-              endDate: '2023-10-31',
-              qualifier: [
-                {
-                  name: 'Oracle Cloud Infrastructure 2023 Sales Specialist',
-                  startDate: '2023-01-01',
-                  endDate: '2023-04-30',
-                },
-                {
-                  name: 'Oracle Analytics 2023 Sales Specialist',
-                  startDate: '2023-05-01',
-                  endDate: '2023-08-31',
-                },
-                {
-                  name: 'Oracle Integration Cloud 2024 Sales Specialist',
-                  startDate: '2023-09-01',
-                  endDate: '2023-10-31',
-                },
-                {
-                  name: 'Data Management 2023 Sales Specialist',
-                  startDate: '2023-11-01',
-                  endDate: '2023-10-31',
-                },
-                {
-                  name: 'Oracle Identity and Access Management 2024 Sales Specialist',
-                  startDate: null,
-                  endDate: '2023-10-31',
-                },
-              ],
-            },
-          ],
-        },
-      ],
-    };
-
-    const partners: PartnerSchema[] = [mockedData];
-    return partners;
-  } catch (error) {
-    console.error('Erro ao obter dados mockados do Parceiro:', error);
-    throw error;
-  }
-}
-
+import { PartnerSchema, PartnerPostSchema } from '../schemas/partner/Partner';
 import axios from 'axios';
 
-export async function getDataPartner(url: string): Promise<PartnerSchema[]> {
+const API_URL: string = 'http://localhost:8080';
+
+export async function parsePartner(partner: any): Promise<PartnerSchema> {
+  return {
+    id: partner.id,
+    companyId: partner.companyId,
+    name: partner.name,
+    adminName: partner.adminName,
+    slogan: partner.slogan,
+    country: partner.country,
+    city: partner.city,
+    address: partner.address,
+    compliance: partner.compliance,
+    credit: partner.credit,
+    status: partner.status,
+    memberType: partner.memberType,
+    insertDate: new Date(partner.insertDate),
+  }
+
+export async function mapPartners(partners: any): Promise<PartnerSchema[]> {
+  return partners ? await partners.map(async (item: any) => parsePartner(item)) : [];
+}
+
+export async function getPartners(): Promise<PartnerSchema[]> {
+    const response = await axios.get(`${API_URL}/partners`);
+    return mapPartners(response.data);
+}
+
+export async function getPartner(id: number): Promise<PartnerSchema> {
+    const response = await axios.get(`${API_URL}/partners/${id}`);
+    return parsePartner(response.data);
+}
+
+export async function createPartner(partner: PartnerSchema): Promise<PartnerSchema> {
+    const response = await axios.post(`${API_URL}/partners`, partner);
+    return parsePartner(response.data);
+}
+
+export async function updatePartner(
+  id: number, 
+  partner: PartnerSchema
+): Promise<PartnerSchema> {
+    const response = await axios.patch(`${API_URL}/partners/${id}`, partner);
+    return parsePartner(response.data);
+}
+
+export async function deletePartner(id: number): Promise<void> {
+    await axios.delete(`${API_URL}/partners/${id}`);
+}
+
+export async function getDashboardData(url: string): Promise<PartnerSchema[]> {
   try {
     // eslint-disable-next-line @typescript-eslint/no-explicit-any
     const response = await axios.get<any[]>(url);

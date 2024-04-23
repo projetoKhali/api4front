6abb4b9940aa2bdf56037016e5315cf2af3e4b0b
Paulo Granthon <paulo.granthon@fatec.sp.gov.br>
2024-04-22 21:55:20 -0300

    style: apply style rules to `PartnerService`

--- src/service/PartnerService.ts
+++ src/service/PartnerService.ts
@@ -25,41 +25,43 @@ export async function parsePartner(partner: any): Promise<PartnerSchema> {
     status: partner.status,
     memberType: partner.memberType,
     insertDate: new Date(partner.insertDate),
-  }
+  };
 }
 
 export async function mapPartners(partners: any): Promise<PartnerSchema[]> {
-  return partners ? await partners.map(async (item: any) => parsePartner(item)) : [];
+  return partners
+    ? await partners.map(async (item: any) => parsePartner(item))
+    : [];
 }
 /* eslint-enable @typescript-eslint/no-explicit-any */
 
 export async function getPartners(): Promise<PartnerSchema[]> {
-    const response = await axios.get(`${API_URL}/partners`);
-    return mapPartners(response.data);
+  const response = await axios.get(`${API_URL}/partners`);
+  return mapPartners(response.data);
 }
 
 export async function getPartner(id: number): Promise<PartnerSchema> {
-    const response = await axios.get(`${API_URL}/partners/${id}`);
-    return parsePartner(response.data);
+  const response = await axios.get(`${API_URL}/partners/${id}`);
+  return parsePartner(response.data);
 }
 
-    const response = await axios.post(`${API_URL}/partners`, partner);
-    return parsePartner(response.data);
 export async function createPartner(
   partner: PartnerPostSchema,
 ): Promise<PartnerSchema> {
+  const response = await axios.post(`${API_URL}/partners`, partner);
+  return parsePartner(response.data);
 }
 
 export async function updatePartner(
-  id: number, 
+  id: number,
   partner: PartnerPatchSchema,
 ): Promise<PartnerSchema> {
-    const response = await axios.patch(`${API_URL}/partners/${id}`, partner);
-    return parsePartner(response.data);
+  const response = await axios.patch(`${API_URL}/partners/${id}`, partner);
+  return parsePartner(response.data);
 }
 
 export async function deletePartner(id: number): Promise<void> {
-    await axios.delete(`${API_URL}/partners/${id}`);
+  await axios.delete(`${API_URL}/partners/${id}`);
 }
 
 /* eslint-disable @typescript-eslint/no-explicit-any */
@@ -69,26 +71,28 @@ export async function getDashboardData(
   try {
     const response = await axios.get<any[]>(url);
 
-    const partners: PartnerDashboardSchema[] = response.data.map((item: any) => ({
-      name: item.name,
-      location: item.location,
+    const partners: PartnerDashboardSchema[] = response.data.map(
+      (item: any) => ({
+        name: item.name,
+        location: item.location,
 
-      tracks: item.tracks.map((trackItem: any) => ({
-        name: trackItem.name,
+        tracks: item.tracks.map((trackItem: any) => ({
+          name: trackItem.name,
 
-        expertises: trackItem.expertises.map((expertiseItem: any) => ({
-          name: expertiseItem.name,
-          startDate: new Date(expertiseItem.startDate),
-          endDate: new Date(expertiseItem.endDate),
+          expertises: trackItem.expertises.map((expertiseItem: any) => ({
+            name: expertiseItem.name,
+            startDate: new Date(expertiseItem.startDate),
+            endDate: new Date(expertiseItem.endDate),
 
-          qualifier: expertiseItem.qualifier.map((qualifierItem: any) => ({
-            name: qualifierItem.name,
-            startDate: new Date(qualifierItem.startDate),
-            endDate: new Date(qualifierItem.endDate),
+            qualifier: expertiseItem.qualifier.map((qualifierItem: any) => ({
+              name: qualifierItem.name,
+              startDate: new Date(qualifierItem.startDate),
+              endDate: new Date(qualifierItem.endDate),
+            })),
           })),
         })),
-      })),
-    }));
+      }),
+    );
     return partners;
   } catch (error) {
     console.error('Erro ao obter dados do Parceiro:', error);

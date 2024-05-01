d0f6691737710e691ebcc8a56959ca6d7be479ce
Paulo Granthon <paulo.granthon@fatec.sp.gov.br>
2024-05-01 02:21:07 -0300

    feat: create `Form` component

---
+++ src/components/form/Form.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <div v-for="(key, index) in Object.keys(data)" :key="index">
+      <label>{{ key }}</label>
+      <input
+        :type="utils.data[key] instanceof Date ? 'date' : typeof data[key]"
+        :value="data[key]"
+        @input="onChangeFunctions[key](data, $event.target.value)"
+      >
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+export default {
+  props: {
+    data: {
+      type: Object,
+      required: true,
+    },
+    onChangeFunctions: {
+      type: Object,
+      required: true,
+    },
+  }
+}
+</script>
+
+<style scoped></style>

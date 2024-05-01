d169de34803006c9a27e9bd75a10df3b52323b29
Paulo Granthon <paulo.granthon@fatec.sp.gov.br>
2024-05-01 03:00:45 -0300

    docs: add comments in `Form` component

--- src/components/form/Form.vue
+++ src/components/form/Form.vue
@@ -1,7 +1,24 @@
 <template>
   <div>
+
+    <!-- use `v-for` para efetuar um loop dentro do template -->
+    <!--     `Object.keys(data)` retorna um array com os nomes de cada atributo do objeto `data` -->
     <div v-for="(key, index) in Object.keys(data)" :key="index">
+
+      <!-- `key` é o nome do atributo -->
+      <!--     chaves duplas `{{ }}` são usadas para inserir valores de variaveis dentro do template -->
       <label>{{ key }}</label>
+
+      <!-- :type define o tipo do input -->
+      <!--    nesse caso estamos chamando a função `getFormFieldInputType` que retorna o tipo do input baseado no valor do atributo -->
+      <!--    mas também é possível usar `:type="typeof data[key]"` caso não seja necessário um tratamento especial (diferenciar apenas text e number por ex) -->
+      <!--    ou `:type="data[key] instanceof Date ? 'date' : typeof data[key]"` para tratar o caso de ser uma data, etc -->
+      <!-- :value define o valor que estará sendo exibido no input ao ser renderizado -->
+      <!--    `data[key]` faz com que o valor seja puxado do atributo de nome `key` do objeto `data` -->
+      <!-- @input é um evento que é disparado quando o valor do input é alterado, assim como o onChange do React -->
+      <!--    `onChangeFunctions[key]` é um objeto que contém funções que são chamadas quando o valor do input é alterado -->
+      <!--    $event.target.value é o valor que foi inserido no input -->
+      <!--    ou seja, estamos chamando a função `onChangeFunctions[key]` passando o objeto `data` e o valor inserido no input -->
       <input
         :type="getFormFieldInputType(data, key)"
         :value="data[key]"

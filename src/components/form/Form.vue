24fe58ea84e7805a2ac1c618f2bbad948a3302dd
Paulo Granthon <paulo.granthon@fatec.sp.gov.br>
2024-05-03 23:59:25 -0300

    chore: update style classes in `form` component

--- src/components/form/Form.vue
+++ src/components/form/Form.vue
@@ -1,19 +1,21 @@
 <template>
   <div class="form-container">
     <h1 v-if="title" class="form-title">{{ title }}</h1>
-    <div
-      v-for="(key, index) in Object.keys(data).filter(
-        key => key in onChangeFunctions,
-      )"
-      :key="index"
-      class="form-row"
-    >
-      <label>{{ key }}</label>
-      <input
-        :type="getFormFieldInputType(data, key)"
-        :value="data[key]"
-        @input="onChangeFunctions[key](data, $event.target.value)"
-      />
+    <div v-if="data" class="form-field-list">
+      <div
+        v-for="(key, index) in Object.keys(data).filter(
+          key => key in onChangeFunctions,
+        )"
+        :key="index"
+        class="form-row"
+      >
+        <label>{{ key }}</label>
+        <input
+          :type="getFormFieldInputType(data, key)"
+          :value="data[key]"
+          @input="onChangeFunctions[key](data, $event.target.value)"
+        />
+      </div>
     </div>
     <ul class="form-button-list" v-if="actions">
       <div
@@ -82,16 +84,15 @@ export default {
   text-align: center;
 }
 
+.form-field-list {
+  display: flex;
+  flex-direction: column;
+}
+
 .form-row {
   display: flex;
   flex-direction: row;
   justify-content: space-evenly;
-  gap: 1rem;
-  width: 100%;
-}
-
-.form-row * {
-  width: 100%;
 }
 
 .form-row label {

f3ca6655e9e5f5ca852bd990262b66d9ba9992be
jonat <lucas.gomes70@fatec.sp.gov.br>
2024-05-05 16:19:32 -0300

    in progress menu

--- src/router/index.ts
+++ src/router/index.ts
@@ -2,8 +2,14 @@ import { createRouter, createWebHistory } from 'vue-router';
 import PartnerDashboard from '../views/PartnerDashboard.vue';
 import TrackDashboard from '../views/TrackDashboard.vue';
 
+<<<<<<< Updated upstream
 const routes = [
   {
+=======
+const routes = {
+  dashboard: [
+    {
+>>>>>>> Stashed changes
     path: '/partner',
     name: 'Partner',
     component: PartnerDashboard,
@@ -12,12 +18,28 @@ const routes = [
     path: '/track',
     name: 'Track',
     component: TrackDashboard,
-  },
-];
+  }  
+],
+  listagem: [
+    {
+      path: '/partner',
+      name: 'Partner',
+      component: PartnerDashboard,
+    },
+    {
+      path: '/track',
+      name: 'Track',
+      component: TrackDashboard,
+    }  
+  ]
+  };
 
 const router = createRouter({
   history: createWebHistory(),
-  routes,
+  routes: [
+    ...routes.dashboard,
+    ...routes.listagem,
+  ]
 });
 
 export default router;

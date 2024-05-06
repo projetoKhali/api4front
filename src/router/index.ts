45e344c68b8f5d6d12c6487614ef62874e4748bd
Paulo Granthon <paulo.granthon@fatec.sp.gov.br>
2024-05-06 01:00:14 -0300

    test: add `FormExample` to `routes`

--- src/router/index.ts
+++ src/router/index.ts
@@ -2,12 +2,18 @@ import { createRouter, createWebHistory } from 'vue-router';
 import PartnerDashboard from '../views/PartnerDashboard.vue';
 import TrackDashboard from '../views/TrackDashboard.vue';
 import TableExample from '../views/TableExample.vue';
+import FormExample from '../views/FormExample.vue';
 
 const routes = [
   {
     path: '/',
     name: 'Table Test',
     component: TableExample,
+  },
+  {
+    path: '/formExample',
+    name: 'Form Popup Example',
+    component: FormExample,
   },
   {
     path: '/partner',

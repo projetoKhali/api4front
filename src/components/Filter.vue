fdb388fbc105e9146698c1d114c396a848229041
Paulo Granthon <paulo.granthon@fatec.sp.gov.br>
2024-05-06 20:01:40 -0300

    feat: create base `Filter` component contract

---
+++ src/components/Filter.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="filter">
+    <div
+      v-for="(field, index) in Object.keys(fields)"
+      :key="index"
+      class="filter-row"
+    >
+      <label>{{ fields[field].title }}</label>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+export type FilterField = {
+  title: string;
+  onChange: (value: string) => void;
+} & (
+  | {
+      dropdown: string[];
+    }
+  | {
+      type: 'text' | 'number' | 'date' | 'checkbox';
+    }
+);
+
+export default {
+  props: {
+    fields: {
+      type: Array as () => FilterField[],
+      required: true,
+    },
+  },
+};
+</script>

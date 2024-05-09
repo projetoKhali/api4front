5129961dc1e1eccb6e7a7419f8cb8a088bd2158d
taniacruzz <tanicruz112@gmail.com>
2024-05-08 22:00:30 -0300

    feat: schema do Partner Metric

---
+++ src/schemas/partner/PartnerMetric.ts
@@ -0,0 +1,10 @@
+export interface PartnerMetricSchema {
+  id: number;
+  name: string;
+  tracks: number;
+  expertises: number;
+  completedExperteses: number;
+  qualifiers: number;
+  timeInterval: number;
+  city: string;
+}

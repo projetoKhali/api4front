6532d264dd77848675f45906a0db03b0c5890dfd
jonat <lucas.gomes70@fatec.sp.gov.br>
2024-05-08 21:47:05 -0300

    in progress Trackmetricsu

---
+++ src/schemas/track/TrackMetrics.ts
@@ -0,0 +1,9 @@
+export interface TrackMetricsSchema{
+    trackId: number;
+    expertiseCount: number;
+    qualifierCount: number;
+    partnerCount: number;
+    expertiseCompletedOnTime:number;
+    qualifierCompletedOnTime:number;
+    qualifierCompletedOnPercentage:number;
+} 

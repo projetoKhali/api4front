70c65114afecd4709e89257626690196dc7536a6
jonat <lucas.gomes70@fatec.sp.gov.br>
2024-05-15 20:16:38 -0300

    chore: ServiceTrackMetrics

--- src/service/TrackMetricService.ts
+++ src/service/TrackMetricService.ts
@@ -1,35 +1,40 @@
-import { TrackSchema } from '../schemas/track/Track';
 import { TrackMetricsSchema } from '../schemas/track/TrackMetrics';
 import axios from 'axios';
 
 const API_URL: string = 'http://localhost:8080';
 
-export async function parseTrackMetrics(tracks:any): Promise<TrackMetricsSchema>{
+/* eslint-disable @typescript-eslint/no-explicit-any */
+export async function parseTrackMetrics(
+  tracks: any,
+): Promise<TrackMetricsSchema> {
   return {
     trackId: tracks.id,
     expertiseCount: tracks.expertiseCount,
-    qualifierCount:tracks.qualifierCount,
+    qualifierCount: tracks.qualifierCount,
     partnerCount: tracks.partnerCount,
     expertiseCompletedOnTime: tracks.expertiseCompletedOnTime,
     qualifierCompletedOnTime: tracks.qualifierCompletedOnTime,
-    qualifierCompletedOnPercentage: tracks.qualifierCompletedOnPercentage
+    qualifierCompletedOnPercentage: tracks.qualifierCompletedOnPercentage,
   };
-  
 }
 
-export async function mapTrackMetrics(tracks: any): Promise<TrackMetricsSchema[]> {
+export async function mapTrackMetrics(
+  tracks: any,
+): Promise<TrackMetricsSchema[]> {
   return tracks
     ? await tracks.map(async (item: any) => parseTrackMetrics(item))
     : [];
 }
 
+/* eslint-enable @typescript-eslint/no-explicit-any */
 export async function getTrackMetrics(): Promise<TrackMetricsSchema[]> {
   const response = await axios.get(`${API_URL}/tracks`);
   return mapTrackMetrics(response.data);
 }
 
-export async function getTrackMetric(trackId:number): Promise<TrackMetricsSchema> {
+export async function getTrackMetric(
+  trackId: number,
+): Promise<TrackMetricsSchema> {
   const response = await axios.get(`${API_URL}/tracks/${trackId}`);
   return parseTrackMetrics(response.data);
-  
 }

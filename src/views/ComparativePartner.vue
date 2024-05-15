da8b86909403a116a751fa001338e7d6dc810c4e
Incivius <marcos.santos232@fatec.sp.gov.br>
2024-05-15 20:15:27 -0300

    feat: create pages

---
+++ src/views/ComparativePartner.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="dashboard-container">
+      <div class="left-side">
+      </div>
+      <div class="right-side">
+    </div>
+    </div>
+  </template>
+  
+  <script setup lang="ts">
+  </script>
+  
+  <style>
+  </style>
+  

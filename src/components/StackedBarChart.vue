e74c6b0c2146a744670971c03fb855d11c69261e
JhonatanLop <jhooliveira.lopes@gmail.com>
2024-05-15 22:08:51 -0300

    feat: Add StackedBarChart component
    The StackedBarChart component is added to the project. This component allows for the creation of stacked bar charts with customizable titles, sections, and data.

---
+++ src/components/StackedBarChart.vue
@@ -0,0 +1,33 @@
+<script setup lang="ts">
+import { onMounted, ref } from 'vue';
+import { Bar } from 'vue-chartjs';
+
+const props = defineProps({
+    title:{
+        type: String,
+        required: true
+    },
+    section:{
+        type: Array<String>,
+        required: true
+    },
+    data:{
+        type: Array<Array<Number>>,
+        required: true
+    }
+})
+
+let chart = ref(null)
+
+// rook que ao montar o componente, ele cria o gráfico
+onMounted(() => {
+
+})
+
+</script>
+
+<template>
+    <div>
+        <canvas id="stackedBarChart"></canvas>
+    </div>
+</template>

93616ed1e587526889ad053734b4a3511d2403f4
JhonatanLop <jhooliveira.lopes@gmail.com>
2024-05-19 18:49:13 -0300

    chore: Refactor StackedBarChart component to improve code structure
    still using mocked data

--- src/components/StackedBarChart.vue
+++ src/components/StackedBarChart.vue
@@ -1,44 +1,90 @@
 <template>
-    <li v-for=" label in props.labels" class="list-bar">
-        // barra de expertizes na track
-        <div class="bar" :style="getBarStyle(0)"> </div>
-        <div class="bar" :style="getBarStyle(1)"> </div>
-        <div class="bar" :style="getBarStyle(2)"> </div>
-    </li>
-    <div class="title">
-        <h2>{{ title }}</h2>
+    <div class="conteiner">
+        <div class="chart">
+            <div v-for="(bar, i) in data" class="bar-container">
+                <div v-for="(segment, j) in bar" class="bar-segment" :style="getBarStyle(i, j)"></div>
+            </div>
+        </div>
+        <div class="title">
+            <h2>{{ title }}</h2>
+        </div>
     </div>
 </template>
 
 <script setup lang="ts">
 
-const props = defineProps({
-    title: {
-        type: String,
-        required: true
-    }, 
-    labels: {
-        type: Array<String>,
-        required: true
-    },
-    data: {
-        type: Array<Array<Number>>,
-        required: true
-    }
-})
+// const props = defineProps({
+//     title: {
+//         type: String,
+//         required: true
+//     },
+//     labels: {
+//         type: Array<String>,
+//         required: true
+//     },
+//     data: {
+//         type: Array<Array<Number>>,
+//         required: true
+//     }
+// })
 
-const getBarStyle = (index: number) => {
-    let barHeight = 0;
-    const dadosVsSampleFactor = 0;
+// exemplo de dado
+const title = 'Track Shell'
+const label = ['Marcos', 'Tania', 'Paulo']
+const data = [[22, 10, 5], [22, 13, 8], [22, 9, 8]]
+
+const getBarStyle = (barIndex: number, segmentIndex: number) => {
+    const dataAtIndex = data[barIndex];
+
+    if (!dataAtIndex || !dataAtIndex[segmentIndex]) {
+        return {};
+    }
+
+    const total = dataAtIndex.reduce((a, b) => a + b, 0);
+    const dataAtSegment = dataAtIndex[segmentIndex];
+
+    if (!total || !dataAtSegment) {
+        return {};
+    }
+
+    const segmentHeight = (dataAtSegment / total) * 100;
+
     return {
-        height: `${barHeight * dadosVsSampleFactor}%`,
-        backgroundColor: index === 0 ? 'blue' : index === 1 ? 'yellow' : 'green',
+        height: `${segmentHeight}%`,
+        backgroundColor: segmentIndex === 0 ? 'blue' : segmentIndex === 1 ? 'yellow' : 'green',
     };
 }
-
 </script>
 
 <style scoped>
+/* TODO: ajustar estilo */
+.conteiner {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    width: 200px;
+    height: 200px;
+}
+
+.bar-container {
+    display: flex;
+    flex-direction: column;
+    justify-content: flex-end;
+    width: 20%;
+    height: 30vh;
+    background: palegoldenrod;
+    margin: 0 10px;
+}
+
+.chart {
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+    list-style-type: none;
+    width: 100%;
+}
+
 .list-bar {
     width: 100%;
     height: 50vh;

d9593ee9db6e6019e6f879feeb463ce32adab9cc
JhonatanLop <jhooliveira.lopes@gmail.com>
2024-05-19 00:09:13 -0300

    feat: Update StackedBarChart component.
    created a function to get a bar style using index and data.

--- src/components/StackedBarChart.vue
+++ src/components/StackedBarChart.vue
@@ -1,10 +1,22 @@
+<template>
+    <li v-for=" label in props.labels" class="list-bar">
+        // barra de expertizes na track
+        <div class="bar" :style="getBarStyle(0)"> </div>
+        <div class="bar" :style="getBarStyle(1)"> </div>
+        <div class="bar" :style="getBarStyle(2)"> </div>
+    </li>
+    <div class="title">
+        <h2>{{ title }}</h2>
+    </div>
+</template>
+
 <script setup lang="ts">
 
 const props = defineProps({
     title: {
         type: String,
         required: true
-    },
+    }, 
     labels: {
         type: Array<String>,
         required: true
@@ -15,12 +27,26 @@ const props = defineProps({
     }
 })
 
+const getBarStyle = (index: number) => {
+    let barHeight = 0;
+    const dadosVsSampleFactor = 0;
+    return {
+        height: `${barHeight * dadosVsSampleFactor}%`,
+        backgroundColor: index === 0 ? 'blue' : index === 1 ? 'yellow' : 'green',
+    };
+}
+
 </script>
 
-<template>
-    <div v-for=" label in props.labels">
-    </div>
-    <div class="title">
-        <h2>{{ title }}</h2>
-    </div>
-</template>
+<style scoped>
+.list-bar {
+    width: 100%;
+    height: 50vh;
+    margin: 0 10px;
+}
+
+.bar {
+    width: 100%;
+    height: 100%;
+}
+</style>

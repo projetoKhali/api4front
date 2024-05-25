844478c209752c8d1ff8728617d6d96f7f711767
Incivius <marcos.santos232@fatec.sp.gov.br>
2024-05-25 10:24:37 -0300

    feat: dashboard comparative

--- src/service/TrackMetricService.ts
+++ src/service/TrackMetricService.ts
@@ -8,7 +8,7 @@ export async function parseTrackMetrics(
   tracks: any,
 ): Promise<TrackMetricsSchema> {
   return {
-    trackId: tracks.id,
+    trackId: tracks.tkId,
     expertiseCount: tracks.expertiseCount,
     qualifierCount: tracks.qualifierCount,
     partnerCount: tracks.partnerCount,
@@ -22,19 +22,19 @@ export async function mapTrackMetrics(
   tracks: any,
 ): Promise<TrackMetricsSchema[]> {
   return tracks
-    ? await tracks.map(async (item: any) => parseTrackMetrics(item))
+    ? await Promise.all(tracks.map(async (item: any) => await parseTrackMetrics(item)))
     : [];
 }
 
 /* eslint-enable @typescript-eslint/no-explicit-any */
 export async function getTrackMetrics(): Promise<TrackMetricsSchema[]> {
   const response = await axios.get(`${API_URL}/trackmetrics`);
-  return mapTrackMetrics(response.data);
+  return await mapTrackMetrics(response.data);
 }
 
 export async function getTrackMetric(
   trackId: number,
 ): Promise<TrackMetricsSchema> {
   const response = await axios.get(`${API_URL}/trackmetrics/${trackId}`);
-  return parseTrackMetrics(response.data);
+  return await parseTrackMetrics(response.data);
 }
--- src/views/ComparativeTracks.vue
+++ src/views/ComparativeTracks.vue
@@ -1,37 +1,209 @@
 <template>
   <div class="dashboard-container">
-    <div class="right-side">
-      <div class="linechart-container">
-        <LineChart :chartData="chartData" />
+      <div class="left-side">
+        <div class="linechart-container-time">
+          <LineChart :chartData="chartDataTime" />
+        </div>
+        <div class="linechart-container-progress">
+          <LineChart :chartData="chartDataProgress" />
+        </div>
+        <div class="linechart-container-count">
+          <LineChart :chartData="chartDataCount" />
+        </div>
       </div>
+      <div class="right-side"> 
+        <div class="bar-chat-dashboard"> 
+          <BarChart :chartData="formattedBarChartData" />
+        </div>
+        <div class="table-dashboard"> 
+          <Table
+            :headers="tableHeadTrack"
+            :initialData="tableBodyTrack"
+          />
+      </div>      
     </div>
   </div>
 </template>
 
 <script setup lang="ts">
+import { onMounted, ref, watch } from 'vue';
 import LineChart from '../components/charts/LineChart.vue';
+import BarChart from '../components/charts/BarChart.vue';
+import Table from '../components/Table.vue';
+import { getTrackMetrics } from '../service/TrackMetricService';
+import { TrackMetricsSchema } from '../schemas/track/TrackMetrics';
+import MultiSelect from 'primevue/multiselect';
 
-const chartData = {
-  labels: ['Janeiro', 'Fevereiro', 'Março', 'Abril', 'Maio'],
-  datasets: [
-    {
-      label: 'Série 1',
-      data: [10, 20, 15, 25, 30], // Valores para o eixo Y
-      borderColor: 'blue', // Cor da linha
-      borderWidth: 2, // Largura da linha
-      fill: false, // Não preencher área abaixo da linha
-    },
-    {
-      label: 'Série 2',
-      data: [15, 25, 20, 30, 35], // Valores para o eixo Y
-      borderColor: 'red', // Cor da linha
-      borderWidth: 2, // Largura da linha
-      fill: false, // Não preencher área abaixo da linha
-    },
-  ],
-};
+const tableBodyTrack = ref<TrackTableRow[]>([]);
+const tracks = ref<TrackMetricsSchema[]>([]);
+const chartDataTime = ref<chartLineType>({ labels: [], datasets: [] });
+const chartDataProgress = ref<chartLineType>({ labels: [], datasets: [] });
+const chartDataCount = ref<chartLineType>({ labels: [], datasets: [] });
+
+const tableHeadTrack = [
+  'Track',
+  'Número de parceiros',
+  'Abandono',
+  'Média de conclusão',
+  'Tempo Médio de conclusão'
+];
+
+const selectedCities = ref();
+const cities = ref([
+  { name: 'New York', code: 'NY' },
+  { name: 'Rome', code: 'RM' },
+  { name: 'London', code: 'LDN' },
+  { name: 'Istanbul', code: 'IST' },
+  { name: 'Paris', code: 'PRS' },
+]);
+
+interface chartLineType {
+  labels: string[];
+  datasets: datasetType[];
+}
+
+interface datasetType {
+  label: string;
+  data: number[];
+  borderColor: string;
+  borderWidth: number;
+  fill: boolean
+}
+
+type TrackTableRow = [
+  string,
+  number,
+  number,
+  number,
+  number
+];
+
+const formattedBarChartData = ref({
+  labels: [] as string[],
+  datasets: [] as { label: string; data: number[]; backgroundColor: string; borderColor: string; borderWidth: number }[],
+});
+
+onMounted(async () => {
+  try {
+    tracks.value = await getTrackMetrics();
+    console.log("tracks loaded", tracks.value);
+
+    const mappedData: TrackTableRow[] = tracks.value.map(track => [
+      `Track ${track.trackId}`,
+      track.partnerCount,
+      track.qualifierCount,
+      track.qualifierCount,
+      track.qualifierCount ?? 0,
+    ]);
+
+    tableBodyTrack.value = mappedData;
+    updateChartData();
+    console.log('chartDataTime', chartDataTime.value);
+  } catch (error) {
+    console.error('Erro ao carregar dados dos tracks:', error);
+  }
+});
+
+function updateChartData() {
+  chartDataTime.value = {
+    labels: tracks.value.map(track => `Track ${track.trackId}`),
+    datasets: [
+      {
+        label: 'Tempo Médio de Conclusão de Expertise',
+        data: tracks.value.map(track => track.expertiseCount ?? 0),
+        borderColor: 'blue',
+        borderWidth: 2,
+        fill: false,
+      },
+      {
+        label: 'Tempo Médio de Conclusão de Qualificador',
+        data: tracks.value.map(track => track.qualifierCount ?? 0),
+        borderColor: 'red',
+        borderWidth: 2,
+        fill: false,
+      }
+    ],
+  };
+
+  chartDataProgress.value = {
+    labels: tracks.value.map(track => `Track ${track.trackId}`),
+    datasets: [
+      {
+        label: 'Progresso de Conclusão de Expertise',
+        data: tracks.value.map(track => track.expertiseCount ?? 0),
+        borderColor: 'blue',
+        borderWidth: 2,
+        fill: false,
+      },
+      {
+        label: 'Progresso de Conclusão de Qualificador',
+        data: tracks.value.map(track => track.qualifierCount ?? 0),
+        borderColor: 'red',
+        borderWidth: 2,
+        fill: false,
+      }
+    ],
+  };
+
+  chartDataCount.value = {
+    labels: tracks.value.map(track => `Track ${track.trackId}`),
+    datasets: [
+      {
+        label: 'Contagem de Expertise',
+        data: tracks.value.map(track => track.expertiseCount ?? 0),
+        borderColor: 'green',
+        borderWidth: 2,
+        fill: false,
+      },
+      {
+        label: 'Contagem de Qualificador',
+        data: tracks.value.map(track => track.qualifierCount ?? 0),
+        borderColor: 'orange',
+        borderWidth: 2,
+        fill: false,
+      }
+    ],
+  };
+
+  formattedBarChartData.value = {
+    labels: tracks.value.map(track => `Track ${track.trackId}`),
+    datasets: [
+      {
+        label: 'Número de Parceiros',
+        data: tracks.value.map(track => track.partnerCount),
+        backgroundColor: 'rgba(75, 192, 192, 0.2)',
+        borderColor: 'rgba(75, 192, 192, 1)',
+        borderWidth: 1,
+      },
+    ],
+  };
+}
 </script>
 
-<style>
-/* Estilos */
+<style scoped>
+.dashboard-container {
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  height: 100vh;
+  padding: 20px;
+  background-color: #ebf2e8;
+  flex: 1;
+}
+
+.left-side {
+  display: flex;
+  flex-direction: column;
+  gap: 20px;
+}
+
+.right-side {
+  display: flex;
+  flex-direction: column;
+  gap: 20px;
+}
+
+.linechart-container-time, .linechart-container-progress, .linechart-container-count, .bar-chat-dashboard, .table-dashboard {
+  flex: 1;
+}
 </style>

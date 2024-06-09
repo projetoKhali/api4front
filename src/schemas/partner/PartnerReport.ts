c34649f5e869c093f8889ca6addaf0248232ea69
Paulo Granthon <paulo.granthon@fatec.sp.gov.br>
2024-06-08 23:58:37 -0300

    feat: create PartnerReportSchema

---
+++ src/schemas/partner/PartnerReport.ts
@@ -0,0 +1,13 @@
+export interface PartnerReportSchema {
+  partner: string;
+  track: string;
+  trackStart: string;
+  trackEnd: string;
+  expertise: string;
+  expertiseStart: string;
+  expertiseEnd: string;
+  qualifier: string;
+  qualifierStart: string;
+  qualifierEnd: string;
+  qualifierExpiration: string;
+}
